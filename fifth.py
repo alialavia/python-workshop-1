80099c6b1d8cacb580ce8e8dface67ebd532b256
Ali Alavi <alialavia@gmail.com>
2018-04-12 13:59:53 -0400

    Read word-weights from a JSON file

--- fifth.py
+++ fifth.py
@@ -1,60 +1,49 @@
 """
-To evaluate the good or bad score of a tweet, we count the number of good and
-bad words in it.
+To evaluate the good or bad score of a tweet, we first tokenize the tweet, and then 
+stemmize each word in our tweet. We also associate each stem with positive and negative values, 
+respectively, using a dictionary.
 
-if a word is good, increase the value of good_words by one
-else if a word is bad, increase the value of bad_words by one
-if good_words > bad_words then it's a good tweet otherwise it's a bad tweet
+Finally, we caculate the average word weight of a tweet, and decide if it's a good or bad one 
+based on that.
 
 """
 import json
-import nltk
+
+from nltk import word_tokenize
 from nltk.stem.porter import *
 
 stemmer = PorterStemmer()
 
 # Break down a string into words
 def get_words(str):
-	return nltk.word_tokenize(str)
+    return word_tokenize(str)
 
 # Calculate the average value of words in list_of_words
-def get_average_word_weight(list_of_words, word_weights):
-	number_of_words = len(list_of_words)
-	sum_of_word_weights = 0.0
-	if number_of_words == 0:
-		return 0.0
-	# Iterate through the words in the tweet string
-	for w in list_of_words:
-	    stemmed_word = stemmer.stem(w)
-	    if stemmed_word in word_weights:
-	        sum_of_word_weights += word_weights[stemmed_word]
-	    #else:
-	        #print ('"' + stemmed_word + '": 0.0,')
+def get_average_word_weight(list_of_words):  
+    number_of_words = len(list_of_words)
+    sum_of_word_weights = 0.0  
+    for w in list_of_words:
+        stemmed_word = stemmer.stem(w)
+        if stemmed_word in word_weights:
+            sum_of_word_weights += word_weights[stemmed_word]
+        else:
+            print ('"' + stemmed_word + '": 0.0,')
 
-	return sum_of_word_weights / number_of_words
+    return sum_of_word_weights / number_of_words
 
-def anaylse_tweet(tweet_string, word_weights):
-	words = get_words(tweet_string)
-	avg_tweet_weight = get_average_word_weight(words, word_weights)
-	print ("The weight of the tweet is " + str(avg_tweet_weight))
+# Initialize word weights and read them from word_weights.json
+word_weights = {}
+with open("word_weights.json") as f: # open the json file, and put its handler in variable f
+    word_weights = json.load(f)      # read the content of the file into 
 
-	if avg_tweet_weight > 0:
-		print ("It's a good tweet")
-	else:
-		print ("It's a bad tweet")
+tweet_string = "Thanks to the historic TAX CUTS that I signed into law, your paychecks are going way UP, your taxes are going way DOWN, and America is once again OPEN FOR BUSINESS!"
 
-# Read tweets from an outside source
-def read_from_files(json_file, tweet_file):
-	word_weights = {}
-	with open(json_file) as f:
-		s = f.read()
-		word_weights = json.loads(s)
+words = get_words(tweet_string)
+avg_tweet_weight = get_average_word_weight(words)
 
-	with open(tweet_file) as f:
-		for tweet in f:
-			print(tweet)
-			anaylse_tweet(tweet, word_weights)
-			print("----------------------")
+print ("The weight of the tweet is " + str(avg_tweet_weight))
 
-read_from_files("word_weights.json", "tweets.txt")
-
+if avg_tweet_weight > 0:
+    print ("It's a good tweet")
+else:
+    print ("It's a bad tweet")
